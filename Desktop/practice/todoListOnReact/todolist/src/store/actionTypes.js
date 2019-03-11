ad43da5600a3557360dbe06a750c32beaa5c27cf
MEDASSSSSSS <280851738@qq.com>
2019-03-12 01:24:39 +0800

    更改一些文件

--- Desktop/practice/todoListOnReact/todolist/src/store/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const CHANGE_INPUT_VALUE = 'change_input_value';
-export const ADD_TODOITEM = 'add_todoitem';
-export const DEL_TODOITEM = 'del_todoitem';
-export const INIT_STATE = 'init_state';
